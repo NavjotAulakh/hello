4a1e8b3fcbd6416df871178138aa31bf51bbf8f7
Navjot Aulakh <navjot.aulakh@uoit.net>
2018-01-25 09:03:37 -0500

    commit

--- hello.c
+++ hello.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    puts("hello canada");
+int main(int argc, char **argv) {
+    printf("There are %d arguments\n");
+
+    char* myarray = malloc(3);
+    *myarray = 'A';
+    *(myarray+1) = 'B';
+    *(myarray+2) = 'C';
+
+    for (int i=0; i < 3; i++) {
+        printf("%d\n", myarray[i]);
+    }
+    for (int i=0; i < 3; i++) {
+        printf("%c\n", myarray[i]);
+    }
+
+
+    printf("hello canada %c\n", myarray[2]);
     return 0;
 }
 
